f44576789ee6d415defae4c653e4a0830a2a0994
Vitaliy P <vitaliypopadiuk@gmail.com>
2025-04-01 16:34:34 +0300

    return 200 ok

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::Write;
 #[allow(unused_imports)]
 use std::net::TcpListener;
 
@@ -9,8 +10,9 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                stream.write("HTTP/1.1 OK\r\n\r\n".as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
